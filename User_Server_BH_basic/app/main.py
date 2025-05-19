cab24c6ae58892fc176b3853058575853d29221a
spnowaczyk <156540992+spnowaczyk@users.noreply.github.com>
2025-05-19 22:43:55 +0200

    cors

--- User_Server_BH_basic/app/main.py
+++ User_Server_BH_basic/app/main.py
@@ -3,8 +3,18 @@ from app.api.endpoints import example, change_settings, send_command, protected,
 from app.db.base import SessionLocal, Base, engine
 from app.db.crud.users import create_user
 from app.auth.device_auth import authorize_device
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For development; restrict this in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include routers
 app.include_router(example.router, tags=["Example"])
